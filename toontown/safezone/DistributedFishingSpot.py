b407942daeaae052825213ac4b14533202d4ae32
Markgasus <markgasus@gmail.com>
2016-11-12 00:25:07 -0500

    Move footstep code to our repo

--- toontown/safezone/DistributedFishingSpot.py
+++ toontown/safezone/DistributedFishingSpot.py
@@ -316,7 +316,7 @@ class DistributedFishingSpot(DistributedObject.DistributedObject):
             self.ripples = Ripples.Ripples(self.nodePath)
             self.ripples.setScale(0.4)
             self.ripples.hide()
-        if self.splashSounds == None:
+        if self.splashSounds is None:
             self.splashSounds = (base.loadSfx('phase_4/audio/sfx/TT_splash1.ogg'), base.loadSfx('phase_4/audio/sfx/TT_splash2.ogg'))
         return
 
